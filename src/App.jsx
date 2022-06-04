9f10485ff9df58af21d24046b1651b0c7a2f3c41
Ruben <ruben.infantes@gmail.com>
2022-06-04 18:08:02 +0200

    LandingPage in progress

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,17 @@
 import React from "react";
+import { Navigate, Route, BrowserRoute as Router, Routes } from "react-router-dom";
+import LandingPage from "./pages/LandingPage";
+import './styles/main.scss';
 
 function App() {
   return (
     <div className="App">
-      <h1>Hola mundo</h1>
+      <Router>
+        <Routes>
+          <Route path="/home" element={<LandingPage />} />
+          <Route path="*" element={<Navigate to="home" />} />
+        </Routes>
+      </Router>
     </div>
   );
 }
