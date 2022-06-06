fe73e969f774e8adf8a834ebea5ddea461739a70
Ruben <ruben.infantes@gmail.com>
2022-06-06 23:50:10 +0200

    landing in progress

--- src/pages/LandingPage/LandingPage.jsx
+++ src/pages/LandingPage/LandingPage.jsx
@@ -2,7 +2,30 @@ import React from 'react'
 
 const LandingPage = () => {
     return (
-        <div>LandingPage</div>
+        <div className='main-home'>
+            <div className='main-home__logo'>
+                <img className='main-home__logo__img' src='/assets/LandingPage/Novantiqua_logo.png' alt='logo' />
+            </div>
+            <div className='main-home__content'>
+                <div className='main-home__content__item'>
+                    <h1 className='main-home__content__item__title'>Sastreria</h1>
+                    <img className='main-home__content__item__img' src='/assets/LandingPage/sastreria-img.jpg.jpg' alt='sastreria' />
+                </div>
+                <div className='main-home__content__item'>
+                    <h1 className='main-home__content__item__title'>Sastreria</h1>
+                    <img className='main-home__content__item__img' src='/assets/LandingPage/sastreria-img.jpg.jpg' alt='sastreria' />
+                </div>
+                <div className='main-home__content__item'>
+                    <h1 className='main-home__content__item__title'>Sastreria</h1>
+                    <img className='main-home__content__item__img' src='/assets/LandingPage/sastreria-img.jpg.jpg' alt='sastreria' />
+                </div>
+            </div>
+            <div className='main-home__contact'>
+
+            </div>
+
+            {/*FooterTODO*/}
+        </div>
     )
 }
 
